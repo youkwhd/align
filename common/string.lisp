0fefc8ea6b7f559c756696204bdb5942fcf28246
youkwhd <lolywk@tutanota.com>
2024-08-03 15:26:43 +0700

    refactor: unnecessary labels

--- common/string.lisp
+++ common/string.lisp
@@ -14,13 +14,11 @@
         ((equal str what) t)
         (t (starts-with? (subseq str 0 (- (length str) 1)) what))))
 
-(defun index (str what &key (from 0))
-  (labels ((recurse (str what &optional (idx from))
-             (let ((sub (subseq str 0 idx)))
-               (cond ((ends-with? sub what) (- idx (length what)))
-                     ((equal sub str) nil)
-                     (t (recurse str what (+ idx 1)))))))
-    (recurse str what)))
+(defun index (str what &key (from-idx 0))
+  (let ((sub (subseq str 0 from-idx)))
+    (cond ((ends-with? sub what) (- from-idx (length what)))
+          ((equal sub str) nil)
+          (t (index str what :from-idx (+ from-idx 1))))))
 
 (defun seperate (str seperator)
   (let ((idx (index str seperator)))
