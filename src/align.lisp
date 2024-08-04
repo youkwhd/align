7ef072061b16b326cbb2422c92d35ec64e34c615
youkwhd <lolywk@tutanota.com>
2024-08-04 22:02:46 +0700

    chore: having to use setf is ..

--- src/align.lisp
+++ src/align.lisp
@@ -6,17 +6,37 @@
 
 (in-package :align)
 
-(defun align (str seperator)
-  (let ((lines (mapcar (lambda (line) (string:seperate line seperator))
-                       (string:split str (format nil "~%")))))
-    lines))
+(defun align (str seperator &key (margin 1))
+  (let* ((lines (mapcar (lambda (line) (string:seperate line seperator))
+                        (string:split str (format nil "~%"))))
+         (max-length (reduce (lambda (acc str-length) (max acc str-length))
+                       lines
+                       :initial-value 0
+                       :key (lambda (line)
+                              (if (<= (length line) 1)
+                                0
+                                (length (first line)))))))
+    (labels ((recurse (&optional (lines lines))
+               (let ((line (first lines)))
+                 (cond ((not line) lines)
+                       ((< (length line) 3) (recurse (rest lines)))
+                       (t
+                        (setf (first line) (string:filln (first line) " " (- max-length (length (first line)))))
+                        (recurse (rest lines)))))))
+      (recurse)
+      lines)))
+
+;; (("aqqqqqqqqb")
+;;  ("qfoo" "=" "b")
+;;  ("k" "=" "val"))
 
 (defun main ()
   (let ((args (align/args:args))))
-    (print (string:seperate "q" "="))
-    (print (string:seperate " =" "="))
-    (print (string:seperate "a=" "="))
-    (print (string:seperate " =b" "="))
-    (print (string:seperate "nothing at all" "="))
-    (print (string:seperate "a=b" "="))
-    (print (align (format nil "foo=b~%k=val") "=")))
+    ;; (print (string:seperate "q" "="))
+    ;; (print (string:seperate " =" "="))
+    ;; (print (string:seperate "a=" "="))
+    ;; (print (string:seperate " =b" "="))
+    ;; (print (string:seperate "nothing at all" "="))
+    ;; (print (string:seperate "a=b" "="))
+    ;; (print (string:seperate "a=b   foo=bar" "=")))
+    (print (align (format nil "aqqqqqqqqb~%qfoo=b~%k=val") "=")))
