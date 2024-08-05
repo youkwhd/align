d343e921149bf4cd16626ebbef4102c6124de040
youkwhd <lolywk@tutanota.com>
2024-08-06 00:28:18 +0700

    chore: remove build.lisp

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/bash
 
-sbcl --script build/build.lisp
+EXE=align
+
+sbcl --non-interactive \
+     --quit \
+     --load common/io.lisp \
+     --load common/string.lisp \
+     --load src/args.lisp \
+     --load src/align.lisp \
+     --eval "(sb-ext:save-lisp-and-die
+               \"$EXE\"
+               :toplevel #'align:main
+               :compression -7
+               :executable t
+               :save-runtime-options t)"
