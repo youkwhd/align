a93bc26a314a82c44e4c7783112baca6488b0975
youkwhd <lolywk@tutanota.com>
2024-08-05 20:13:13 +0700

    chore: proper args pass

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ sbcl --non-interactive \
      --load common/string.lisp \
      --load src/args.lisp \
      --load src/align.lisp \
-     --eval "((lambda () (format t \"~%[$0 ::]~%\") (align:main)))" $@
+     --eval "((lambda () (format t \"~%[$0 ::]~%\") (align:main)))" -- $@
