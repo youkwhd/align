906b36c6de42510e3df5159d7c95760b92d4988c
youkwhd <lolywk@tutanota.com>
2024-08-06 00:55:49 +0700

    refactor: remove equal comparisons

--- common/string.lisp
+++ common/string.lisp
@@ -12,19 +12,19 @@
 (in-package #:string)
 
 (defun ends-with? (str what)
-  (cond ((equal str "") nil)
-        ((equal str what) t)
+  (cond ((string= str "") nil)
+        ((string= str what) t)
         (t (ends-with? (subseq str 1) what))))
 
 (defun starts-with? (str what)
-  (cond ((equal str "") nil)
-        ((equal str what) t)
+  (cond ((string= str "") nil)
+        ((string= str what) t)
         (t (starts-with? (subseq str 0 (- (length str) 1)) what))))
 
 (defun index (str what &key (from-idx 0))
   (let ((sub (subseq str 0 from-idx)))
     (cond ((ends-with? sub what) (- from-idx (length what)))
-          ((equal sub str) nil)
+          ((string= sub str) nil)
           (t (index str what :from-idx (+ from-idx 1))))))
 
 (defun filln (str val n)
